0659b633f8ba7bf6ee686925cdf25afd02a326e7
Thomas Parratt <tparratt@c1r5p10.hive.fi>
2024-06-19 14:30:46 +0300

    fixed more problems and normed

--- expansion2.c
+++ expansion2.c
@@ -6,66 +6,56 @@
 /*   By: tparratt <tparratt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:21:50 by tparratt          #+#    #+#             */
-/*   Updated: 2024/06/18 17:53:02 by tparratt         ###   ########.fr       */
+/*   Updated: 2024/06/19 14:28:21 by tparratt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	get_len(char *s, int start, int len) 
+static int	get_len(char *s, int start, int len)
 {
-	if (start > 0 && s[start - 1] == '$') 
+	if (start > 0 && s[start - 1] == '$')
 	{
-		if (is_whitespace(s[len])) // e.g. "hello $ hello"
+		if (is_whitespace(s[len]))
 		{
-			while (s[len] != '$' && s[len] != '\0' && (ft_isalnum(s[len]) || s[len] == '_' || s[len] == '?' || is_whitespace(s[len]))) 
+			while (s[len] != '$' && s[len] != '\0' && (ft_isalnum(s[len])
+					|| s[len] == '_' || s[len] == '?' || is_whitespace(s[len])))
+				len++;
+		}
+		else
+		{
+			while (s[len] != '$' && s[len] != '\0' && (ft_isalnum(s[len])
+					|| s[len] == '_' || s[len] == '?'))
 			{
 				len++;
-				if (s[len] == '?') 
-				{
-					len++;
+				if ((s[len] == '?' && s[len - 1] == '?')
+					|| (ft_isalnum(s[len]) && s[len - 1] == '?'))
 					break ;
-				}
 			}
 		}
-		else // e.g. "$USER hello"
-		{
-			while (s[len] != '$' && s[len] != '\0' && (ft_isalnum(s[len]) || s[len] == '_' || s[len] == '?')) 
-			{
-				len++;
-				if (s[len] == '?') 
-				{
-					len++;
-					break ;
-				}
-			}
-		}
-	} 
-	else 
-	{
-		while (s[len] != '$' && s[len] != '\0') 
-			len++;
 	}
-	return(len - start);
+	else
+		while (s[len] != '$' && s[len] != '\0')
+			len++;
+	return (len - start);
 }
 
-char *get_substring(char *s, int j) 
+char	*get_substring(char *s, int j)
 {
-    int		start;
-    int		len;
+	int		start;
+	int		len;
 	char	*substring;
-	
+
 	start = j;
 	len = j;
-    if (start < 0 || start >= (int)ft_strlen(s)) 
-        return(ft_strdup(""));
-    len = get_len(s, start, len);
-    substring = ft_substr(s, start, len);
-    if (!substring) 
-        void_malloc_failure();
-    return(substring);
+	if (start < 0 || start >= (int)ft_strlen(s))
+		return (ft_strdup(""));
+	len = get_len(s, start, len);
+	substring = ft_substr(s, start, len);
+	if (!substring)
+		void_malloc_failure();
+	return (substring);
 }
-
 
 void	dup_or_join(char **new_tokens, int loop, int i, char *str)
 {
