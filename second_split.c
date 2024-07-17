b7588aae8c7e258453064a5edfadc5596c42bddc
SpaMillie <militsaspasic@gmail.com>
2024-07-17 14:42:38 +0300

    norminetted second_split

--- second_split.c
+++ second_split.c
@@ -3,97 +3,115 @@
 /*                                                        :::      ::::::::   */
 /*   second_split.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tparratt <tparratt@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 18:00:50 by tparratt          #+#    #+#             */
-/*   Updated: 2024/07/16 18:02:33 by tparratt         ###   ########.fr       */
+/*   Updated: 2024/07/17 14:40:16 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	second_splitting(t_mini *line)
+static int	is_it_meta(char c, char other_c, int option)
 {
-	int	prev_j;
-	int	i;
-	int	j;
-	int	k;
-
-	i = 0;
-	k = 0;
-	while (line->element[i] != NULL)
+	if (c == '>' || c == '<' || c == '|')
 	{
-		j = 0;
-		while (line->element[i][j] != '\0')
+		if (option == 0)
+			return (1);
+		else
 		{
-			prev_j = j;
-			if (line->element[i][j] == '>' || line->element[i][j] == '<' || line->element[i][j] == '|')
-			{
-				if ((line->element[i][j] == '>' && line->element[i][j + 1] == '>') || (line->element[i][j] == '<' &&  line->element[i][j + 1] == '<'))
-					j++;
-				j++;
-			}
-			else
-			{
-				while (line->element[i][j] != '\0' && line->element[i][j] != '>' && line->element[i][j] != '<' && line->element[i][j] != '|')
-				{
-					if (line->element[i][j] == '\'' || line->element[i][j] == '\"')
-						j = ft_skip(line->element[i], j);
-					else
-						j++;
-				}
-			}
-			line->metaed[k++] = ft_substr(line->element[i], prev_j, j - prev_j);
-		    if (line->metaed[k - 1] == NULL)
-				malloc_failure_without_token(line);
+			if ((c == '<' && other_c == '<') || (c == '>' && other_c == '>'))
+				return (1);
+			return (0);
 		}
-		i++;	
 	}
-	line->metaed[k] = NULL;
+	return (0);
 }
 
-static int	w_count(t_mini *line)
+static void	split_single(t_mini *line, char *s, int i)
 {
-	int	i;
-	int	j;
-	int	words;
+	int	prev_i;
 
-	i = 0;
-	words = 0;
-	while (line->element[i] != NULL)
+	while (s[i] != '\0')
 	{
-		j = 0;
-		while (line->element[i][j] != '\0')
+		prev_i = i;
+		if (is_it_meta(s[i], s[i + 1], 0))
 		{
-			if (line->element[i][j] == '>' || line->element[i][j] == '<' || line->element[i][j] == '|')
+			if (is_it_meta(s[i], s[i + 1], 1))
+				i++;
+			i++;
+		}
+		else
+		{
+			while (s[i] != '\0' && !is_it_meta(s[i], s[i + 1], 0))
 			{
-				if ((line->element[i][j] == '>' && line->element[i][j + 1] == '>') || (line->element[i][j] == '<' &&  line->element[i][j + 1] == '<'))
-					j++;
-				j++;
-				words++;
-			}
-			else
-			{
-				while (line->element[i][j] != '\0' && line->element[i][j] != '>' && line->element[i][j] != '<' && line->element[i][j] != '|')
-				{
-					if (line->element[i][j] == '\'' || line->element[i][j] == '\"')
-						j = ft_skip(line->element[i], j);
-					else
-						j++;
-				}
-				words++;
+				if (s[i] == '\'' || s[i] == '\"')
+					i = ft_skip(s, i);
+				else
+					i++;
 			}
 		}
+		line->metaed[line->i++] = ft_substr(s, prev_i, i - prev_i);
+		if (line->metaed[line->i - 1] == NULL)
+			malloc_failure_without_token(line);
+	}
+}
+
+static void	second_splitting(t_mini *line)
+{
+	int	i;
+
+	i = 0;
+	line->i = 0;
+	while (line->element[i] != NULL)
+	{
+		split_single(line, line->element[i], 0);
 		i++;
+	}
+	line->metaed[line->i] = NULL;
+}
+
+static int	w_count(char *s, int words)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (is_it_meta(s[i], s[i + 1], 0))
+		{
+			if (is_it_meta(s[i], s[i + 1], 1))
+				i++;
+			i++;
+			words++;
+		}
+		else
+		{
+			while (s[i] != '\0' && !is_it_meta(s[i], s[i + 1], 0))
+			{
+				if (s[i] == '\'' || s[i] == '\"')
+					i = ft_skip(s, i);
+				else
+					i++;
+			}
+			words++;
+		}
 	}
 	return (words);
 }
 
 int	second_split(t_mini *line)
 {
+	int	i;
 	int	words;
 
-	words = w_count(line);
+	i = 0;
+	words = 0;
+	while (line->element[i] != NULL)
+	{
+		words = w_count(line->element[i], words);
+		i++;
+	}
 	line->metaed = (char **)malloc(sizeof(char *) * (words + 1));
 	if (!line->metaed)
 		malloc_failure_without_token(line);
